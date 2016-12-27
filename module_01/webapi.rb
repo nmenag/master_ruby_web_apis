e1a2a0d87ba45076dfd356eaf0fd4f445d88bf06
Nicolas Mena <nmena.garzon@gmail.com>
2016-12-26 22:43:58 -0500

    Add http verbs GET, POST,PUT, DELETE, PATCH and  OPTIONS

--- module_01/webapi.rb
+++ module_01/webapi.rb
@@ -38,16 +38,83 @@ get '/' do
   'Master Ruby Web APIs - Chapter 2'
 end
 
+# webapi.rb
+# ...
+# get '/users'
+# get '/users/:first_name'
+# head '/users'
+# post '/users'
+# put '/users/:first_name'
+# patch '/users/:first_name'
+# delete '/users/:first_name'
 
-# Looping through the list of routes for which # we want to return a JSON representation.
-['/users', '/users.json'].each do |path|
-  get path do
+
+get '/users' do
+  type = accepted_media_type
+  if type == 'json'
+    content_type 'application/json'
     users.map { |name, data| data.merge(id: name) }.to_json
+  elsif type == 'xml'
+    content_type 'application/xml'
+    Gyoku.xml(users: users)
   end
 end
 
-# Defining /users.xml with the application/xml media type # and simply calling Gyoku on our users hash with a root # element 'users'.
-get '/users.xml' do
-  content_type 'application/xml'
-  Gyoku.xml(users: users)
+get '/users/:first_name' do |first_name|
+  type = accepted_media_type
+
+  if type == 'json'
+    content_type 'application/json'
+    users[first_name.to_sym].merge(id: first_name).to_json
+  elsif type == 'xml'
+    content_type 'application/xml'
+    Gyoku.xml(first_name => users[first_name.to_sym])
+  end
+end
+
+post '/users' do
+  user = JSON.parse(request.body.read)
+  users[user['first_name'].downcase.to_sym] = user
+  url = "http://localhost:4567/users/#{user['first_name']}"
+  response.headers['Location'] = url
+  status 201
+end
+
+put '/users/:first_name' do |first_name|
+  user = JSON.parse(request.body.read)
+  existing = users[first_name.to_sym]
+  users[first_name.to_sym] = user
+  status existing ? 204 : 201
+end
+
+patch '/users/:first_name' do |first_name|
+  type = accepted_media_type
+  user_client = JSON.parse(request.body.read)
+  user_server = users[first_name.to_sym]
+  user_client.each do |key, value|
+    user_server[key.to_sym] = value
+  end
+
+  if type == 'json'
+    content_type 'application/json'
+    user_server.merge(id: first_name).to_json
+  elsif type == 'xml'
+    content_type 'application/xml'
+    Gyoku.xml(first_name => user_server)
+  end
+end
+
+delete '/users/:first_name' do |first_name|
+  users.delete(first_name.to_sym)
+  status 204
+end
+
+options '/users' do
+  response.headers['Allow'] = 'HEAD,GET,POST'
+  status 200
+end
+
+options '/users/:first_name' do
+  response.headers['Allow'] = 'GET,PUT,PATCH,DELETE'
+  status 200
 end
